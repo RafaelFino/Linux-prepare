7243fc5d347037c5103529d4f64e95643f0191ad
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-02-23 19:12:32 -0300

    Include note-plataforma script

---
+++ note-prepare.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#update
+sudo apt update -y
+sudo apt install -y git vim zsh wget unzip jq telnet curl htop terminator docker docker-compose python3 python3-pip kate mousepad
+sudo apt autoclean -y
+sudo apt autoremove -y
+
+#add user to sudo-docker
+sudo usermod -aG docker $USER
+
+#install vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+#install micro
+curl https://getmic.ro | bash
+sudo mv micro /usr/bin
+
+#install zsh/vim fonts
+git clone --depth=1 https://github.com/powerline/fonts.git
+./fonts/install.sh
+rm -rf fonts
+
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
+./nerd-fonts/install.sh
+rm -rf nerd-fonts
+
+#install exa (new LS)
+wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+sudo unzip -o -j exa-linux-x86_64-v0.10.1.zip "bin/exa" -d /usr/bin
+rm exa-linux-x86_64-v0.10.1.zip
+
+#Install oh-my-tmux
+git clone --depth=1 https://github.com/gpakosz/.tmux.git ~/.tmux
+cp .tmux.conf.local ~/.tmux
+cp start-tmux.sh ~/
+
+#change dir to HOME
+cd
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g' .zshrc
+echo 'alias ls="exa -hHBmgaFl --git"' >> ~/.zshrc
+echo 'alias t="tmux"' >> ~/.zshrc
+echo 'alias ta="t a -t"' >> ~/.zshrc
+echo 'alias tls="t ls"' >> ~/.zshrc
+echo 'alias tn="t new -t"' >> ~/.zshrc
+
+echo set nu >> ~/.vim_runtime/my_configs.vim
+echo zsh >> ~/.bashrc
+
+echo 'Done!'
