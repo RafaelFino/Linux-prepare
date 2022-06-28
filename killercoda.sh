3b62a5909bc8f2a2a82fa28e793e4c5031a5c6e1
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-06-28 19:47:21 -0300

    remove exa.zip from killercoda.sh

--- killercoda.sh
+++ killercoda.sh
@@ -16,6 +16,7 @@ mv micro /usr/bin
 EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
 unzip -qo exa.zip bin/exa -d /usr/local
+rm exa.zip
 
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
