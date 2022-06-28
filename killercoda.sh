03ac76af0324cf616b19918dc0fb00c69930dc9e
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-06-28 19:43:11 -0300

    some fixes

--- killercoda.sh
+++ killercoda.sh
@@ -12,15 +12,16 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 curl https://getmic.ro | bash
 mv micro /usr/bin
 
+#install exa
 EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
-unzip -q exa.zip bin/exa -d /usr/local
+unzip -qo exa.zip bin/exa -d /usr/local
 
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"frisk\"/g' .zshrc
-echo 'alias ls="exa -hHBmgaFl --git"' >> ~/.zshrc
+echo 'alias ls="exa -hHbmgaFl"' >> ~/.zshrc
 echo set nu >> ~/.vim_runtime/my_configs.vim
 
 echo 'Done!'
