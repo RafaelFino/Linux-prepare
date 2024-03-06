bf956482abbf18f45e43d8757dbfb5f0ce3403b7
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-03-05 21:20:58 -0300

    Update note-prepare.sh

--- note/note-prepare.sh
+++ note/note-prepare.sh
@@ -2,12 +2,21 @@
 
 #update
 sudo apt update -y
-sudo apt install -y git vim zsh wget unzip jq telnet curl htop terminator docker.io docker-compose python3 python3-pip kate mousepad exa micro btop batcat 
+sudo apt install -y git vim zsh wget unzip jq telnet curl htop terminator docker.io docker-compose python3 python3-pip kate mousepad exa micro btop batcat apt-transport-https
 sudo apt autoclean -y
 sudo apt autoremove -y
 
 #add user to sudo-docker
 sudo usermod -aG docker $USER
+
+#install vscode
+sudo apt-get install gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt update -y
+sudo apt install code
 
 #change dir to HOME
 cd
