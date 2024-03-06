b31118ede3f9929777dc296e095f05263631e7ff
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-03-05 21:15:14 -0300

    Update note-prepare.sh

--- note/note-prepare.sh
+++ note/note-prepare.sh
@@ -2,20 +2,19 @@
 
 #update
 sudo apt update -y
-sudo apt install -y git vim zsh wget unzip jq telnet curl htop terminator docker docker-compose python3 python3-pip kate mousepad exa
+sudo apt install -y git vim zsh wget unzip jq telnet curl htop terminator docker.io docker-compose docker-compose-v2 docker-doc podman-docker python3 python3-pip kate mousepad exa micro btop
 sudo apt autoclean -y
 sudo apt autoremove -y
 
 #add user to sudo-docker
 sudo usermod -aG docker $USER
 
+#change dir to HOME
+cd
+
 #install vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-#install micro
-curl https://getmic.ro | bash
-sudo mv micro /usr/bin
 
 #install zsh/vim fonts
 git clone --depth=1 https://github.com/powerline/fonts.git
@@ -31,20 +30,14 @@ git clone --depth=1 https://github.com/gpakosz/.tmux.git ~/.tmux
 cp .tmux.conf.local ~/.tmux
 cp start-tmux.sh ~/
 
-#change dir to HOME
-cd
-
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g' .zshrc
-echo 'alias ls="exa -hHbmgaFl --time-style=long-iso --icons"' >> ~/.zshrc
-echo 'alias t="tmux"' >> ~/.zshrc
-echo 'alias ta="t a -t"' >> ~/.zshrc
-echo 'alias tls="t ls"' >> ~/.zshrc
-echo 'alias tn="t new -t"' >> ~/.zshrc
+
+echo 'alias ls="eza -hHbmgaFlT -L 1 --time-style=long-iso --icons"' >> ~/.zshrc
+echo 'alias lt="eza -hHbmgaFlT -L 4 --time-style=long-iso --icons"' >> ~/.zshrc
 
 echo set nu >> ~/.vim_runtime/my_configs.vim
-echo zsh >> ~/.bashrc
 
 echo 'Done!'
