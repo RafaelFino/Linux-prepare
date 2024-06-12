888313d98af574de68d24214d4028f754b1a40fe
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-06-12 11:53:17 -0300

    Update webcam-drivers.sh

--- note/webcam-drivers.sh
+++ note/webcam-drivers.sh
@@ -2,7 +2,7 @@
 sudo add-apt-repository ppa:oem-solutions-group/intel-ipu6
 sudo apt update -y
 sudo apt install libcamhal-ipu6ep0
-sudo apt install linux-modules-ipu6-generic-hwe-22.04
-sudo apt install linux-modules-ivsc-generic-hwe-22.04 
+sudo apt install linux-modules-ipu6-generic-hwe-24.04
+sudo apt install linux-modules-ivsc-generic-hwe-24.04 
 sudo apt install sudo apt install libcamhal-ipu6ep0   
 sudo apt full-upgrade -y            
