33eb17265a6bdb67e588fcdd8518f679118dcefb
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-07-28 01:24:13 -0300

    Improve

--- note/note-prepare.sh
+++ note/note-prepare.sh
@@ -2,11 +2,10 @@
 
 # update and add keys
 sudo apt update -y
-sudo apt install wget git zsh gpg
+sudo apt install wget git zsh gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 sudo apt update -y
 sudo apt upgrade -y
