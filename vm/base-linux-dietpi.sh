0480394d642a0b7f4a3298096db1c0a154190f60
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-08-02 10:50:11 -0300

    VM script to dietpi linux

---
+++ vm/base-linux-dietpi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# update and add keys
+sudo apt update -y
+sudo apt upgrade -y
+
+# install packages
+sudo apt install -y wget git zsh gpg vim unzip jq telnet curl htop btop python3-pip exa micro apt-transport-https zlib1g dotnet-sdk-8.0 dotnet-runtime-8.0 aspnetcore-runtime-8.0 sqlite3 docker.io docker-compose
+sudo apt autoclean -y
+sudo apt autoremove -y
+
+# add user to sudo-docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+# newgrp docker
+
+
+# install vim awesome
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo set nu >> ~/.vim_runtime/my_configs.vim
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g' .zshrc
+
+#e[x|z]a
+echo 'alias ls="eza -hHbmgalT -L 1 --time-style=long-iso --icons"' >> ~/.zshrc
+echo 'alias lt="eza -hHbmgalT -L 4 --time-style=long-iso --icons"' >> ~/.zshrc
+
+#golang
+echo 'Installing golang'
+wget "https://go.dev/dl/$(curl https://go.dev/VERSION\?m=text | head -n 1).linux-$(dpkg --print-architecture).tar.gz" -O golang.tar.gz  
+sudo tar -C /usr/local -xzf golang.tar.gz
+rm golang.tar.gz
+echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+
+echo 'Done! '
