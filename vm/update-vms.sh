fd64a6efb332959798781f464e6952ceb8c148fb
Fino <rafael.gottardi@gmail.com>
2024-08-28 22:26:28 +0000

    Include a lot of improvments

--- vm/update-vms.sh
+++ vm/update-vms.sh
@@ -2,11 +2,13 @@
 set -euo pipefail
 
 for p in `cat inventory`; do
-        echo ">> $p"
-
-        ssh dev@$p 'get-keys'
-		ssh dev@$p 'sudo apt update -y && sudo apt upgrade -y && sudo reboot' &
-
-        echo "<< $p"
+	echo ">> $p"
+	# scp get-keys dev@$p:/usr/local/bin
+	# ssh dev@$p get-keys
+	ssh dev@$p 'sudo locale-gen pt_BR.UTF-8 && sudo dpkg-reconfigure locales'	
+	# ssh dev@$p 'sudo apt update -y && sudo apt upgrade -y && sudo reboot' &
+	# ssh dev@$p 'sudo reboot'	
+	# ssh dev@$p 'rm ~/.bash_history && rm ~/.zsh_history'
+	echo "<< $p"
 done
 echo "Done!"
