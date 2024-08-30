755f87b54853a65d8c4b62d38c7e089318c4db4f
Fino <rafael.gottardi@gmail.com>
2024-08-30 22:59:32 +0000

    Improvments

--- vm/update-vms.sh
+++ vm/update-vms.sh
@@ -5,10 +5,11 @@ for p in `cat inventory`; do
 	echo ">> $p"
 	# scp get-keys dev@$p:/usr/local/bin
 	# ssh dev@$p get-keys
-	ssh dev@$p 'sudo locale-gen pt_BR.UTF-8 && sudo dpkg-reconfigure locales'	
+	# ssh dev@$p 'sudo locale-gen pt_BR.UTF-8 && sudo dpkg-reconfigure locales'	
 	# ssh dev@$p 'sudo apt update -y && sudo apt upgrade -y && sudo reboot' &
 	# ssh dev@$p 'sudo reboot'	
 	# ssh dev@$p 'rm ~/.bash_history && rm ~/.zsh_history'
+	ssh dev@$p "rm sudo && echo '192.168.1.8 storage' | sudo tee -a /etc/hosts"
 	echo "<< $p"
 done
 echo "Done!"
