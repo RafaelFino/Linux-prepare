a11811d2185c785fc7326e73f7c43395fa2efcc5
Fino <rafael.gottardi@gmail.com>
2024-10-02 02:16:47 +0000

    Improvments

--- vm/create-shares.sh
+++ vm/create-shares.sh
@@ -3,11 +3,13 @@ set -euo pipefail
 
 for p in `cat inventory`; do
 	echo ">> $p - Creating shares"
-	#ssh dev@$p "sudo apt install -y smbclient cifs-utils"
-	ssh dev@$p "sudo mkdir /media/share"	
-	ssh dev@$p "sudo chmod 777 /media/share"
-	ssh dev@$p "sudo chown dev:dev /media/share"	
-	ssh dev@$p "echo '//192.168.1.8/plataforma   /media/share  cifs    guest,rw,noforceuid,noforcegid,file_mode=0777,dir_mode=0777,noperm        0       0' | sudo tee -a /etc/fstab"
+	ssh dev@$p "sudo apt install -y nfs-common"
+	#ssh dev@$p "sudo chmod 777 /media/share"
+	#ssh dev@$p "sudo chown dev:dev /media/share"	
+	ssh dev@$p "echo 'storage:/plataforma   /media/share  nfs	defaults	0	0' | sudo tee -a /etc/fstab"
+	ssh dev@$p "sed '/192.168.1.8/d' /etc/fstab | sudo tee /etc/fstab"
+	#ssh dev@$p "echo '//192.168.1.8/plataforma   /media/share  cifs    guest,rw,noforceuid,noforcegid,file_mode=0777,dir_mode=0777,noperm        0       0' | sudo tee -a /etc/fstab"
+	ssh dev@$p "sudo umount /media/share"
 	ssh dev@$p "sudo systemctl daemon-reload"
 	ssh dev@$p "sudo mount -a"
 	echo "<< $p - Done!"
--- vm/update-vms.sh
+++ vm/update-vms.sh
@@ -2,14 +2,11 @@
 set -euo pipefail
 
 for p in `cat inventory`; do
-	echo ">> $p"
-	# scp get-keys dev@$p:/usr/local/bin
-	# ssh dev@$p get-keys
-	# ssh dev@$p 'sudo locale-gen pt_BR.UTF-8 && sudo dpkg-reconfigure locales'	
-	# ssh dev@$p 'sudo apt update -y && sudo apt upgrade -y && sudo reboot' &
-	# ssh dev@$p 'sudo reboot'	
-	# ssh dev@$p 'rm ~/.bash_history && rm ~/.zsh_history'
-	ssh dev@$p "rm sudo && echo '192.168.1.8 storage' | sudo tee -a /etc/hosts"
+	echo ">> $p - Update"
+	scp get-keys dev@$p:
+	ssh dev@$p "sudo mv get-keys /usr/local/bin && get-keys"
+	ssh dev@$p 'sudo apt update -y && sudo apt upgrade -y' &
+	#ssh dev@$p 'rm ~/.bash_history && rm ~/.zsh_history'
 	echo "<< $p"
 done
 echo "Done!"
