28bcb049b973bf1bece433091bae45f8c14d8d36
RafaelFino <rafael.gottardi@gmail.com>
2025-05-02 14:21:23 -0300

    WIP

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -4,3 +4,8 @@
 wget "https://go.dev/dl/$(curl https://go.dev/VERSION\?m=text | head -n 1).linux-$(dpkg --print-architecture).tar.gz" -O golang.tar.gz
 tar -C /usr/local -xzf golang.tar.gz
 rm golang.tar.gz
+
+#check if go path is in /etc/environment
+if ! grep -q "PATH" /etc/environment; then
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/environment
+fi
--- scripts/user-env.sh
+++ scripts/user-env.sh
@@ -2,10 +2,16 @@
 
 # change directory to home
 
-# install env
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-echo set nu >> ~/.vim_runtime/my_configs.vim
+# install vim
+# check if vim is installed
+if [ -d ~/.vim_runtime ]; then
+then
+    echo "Vim is already installed"
+else
+    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+    echo set nu >> ~/.vim_runtime/my_configs.vim
+fi
 
 # bash 
 if [ -d ~/.oh-my-bash ]; then
@@ -15,7 +21,6 @@ else
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
     echo 'alias ls="eza -hHbmgalT -L 1 --time-style=long-iso --icons"' >> ~/.bashrc
     echo 'alias lt="eza -hHbmgalT -L 4 --time-style=long-iso --icons"' >> ~/.bashrc
-    echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 fi
 
 # zsh
@@ -26,6 +31,5 @@ else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"frisk\"/g' ~/.zshrc
     echo 'alias ls="eza -hHbmgalT -L 1 --time-style=long-iso --icons"' >> ~/.zshrc
-    echo 'alias lt="eza -hHbmgalT -L 4 --time-style=long-iso --icons"' >> ~/.zshrc
-    echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+    echo 'alias lt="eza -hHbmgalT -L 4 --time-style=long-iso --icons"' >> ~/.zshrc    
 fi
