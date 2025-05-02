bdc3e585c8f15d1a15f686c2d177c8bd3618b802
RafaelFino <rafael.gottardi@gmail.com>
2025-05-02 14:27:14 -0300

    WIP

--- scripts/user-env.sh
+++ scripts/user-env.sh
@@ -17,7 +17,7 @@ if [ -d ~/.oh-my-bash ]; then
     echo "Oh My Bash is already installed"
 else
     echo "Installing Oh My Bash..."  
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+    curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | bash
     echo 'alias ls="eza -hHbmgalT -L 1 --time-style=long-iso --icons"' >> ~/.bashrc
     echo 'alias lt="eza -hHbmgalT -L 4 --time-style=long-iso --icons"' >> ~/.bashrc
 fi
@@ -27,7 +27,7 @@ if [ -d ~/.oh-my-zsh ]; then
     echo "Oh My Zsh is already installed"
 else
     echo "Installing Oh My Zsh..."  
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
     sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"frisk\"/g' ~/.zshrc
     echo 'alias ls="eza -hHbmgalT -L 1 --time-style=long-iso --icons"' >> ~/.zshrc
     echo 'alias lt="eza -hHbmgalT -L 4 --time-style=long-iso --icons"' >> ~/.zshrc    
