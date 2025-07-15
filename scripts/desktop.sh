23adf22a5e37c809c2c7d496be1542a338261f02
RafaelFino <rafael.gottardi@gmail.com>
2025-07-15 00:59:58 -0300

    Update desktop script file

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -10,6 +10,10 @@ git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
 ./nerd-fonts/install.sh
 rm -rf nerd-fonts
 
+#install ms core fonts
+sudo apt install -y ttf-mscorefonts-installer
+
+
 # install vscode application on desktop
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 -O vscode.deb
 sudo dpkg -i vscode.deb
@@ -22,13 +26,6 @@ sudo apt install -y terminator tilix alacritty tmux kitty
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
-
-# Install brave browser
-wget -qO - https://brave-browser-archive.s3.brave.com/brave-core.asc | sudo gpg --dearmor -o /usr/share/keyrings/brave-browser.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser.gpg] https://brave-browser-archive.s3.brave.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/brave-browser.list > /dev/null
-sudo apt update
-sudo apt install -y brave-browser
-rm brave-browser.gpg
 
 # Check installs
 sudo apt install -y -f
